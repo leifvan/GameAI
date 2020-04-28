5f103752009f7cda75aedd27d13642a4b4ab787a
Leif Van Holland <holland@uni-bonn.de>
2020-04-28 19:57:16 +0200

    improve speed with some numba rewrites

--- games/connect_four/connect_four.py
+++ games/connect_four/connect_four.py
@@ -3,6 +3,7 @@ from itertools import groupby
 from operator import itemgetter
 from random import choice
 from functools import partial
+from numba import njit
 
 from game import TurnBasedGame, run_multiple_games
 
@@ -14,8 +15,9 @@ class ConnectFourGame(TurnBasedGame):
         return np.zeros((6, 7), dtype=int)
 
     @staticmethod
-    def victory_condition(game_state):
-        zero_padded_state = np.pad(game_state, ((0, 1), (0, 1)), mode='constant')
+    def victory_condition(game_state, zero_padded_state=np.zeros((7,8))):
+        #zero_padded_state = np.pad(game_state, ((0, 0), (1, 1)), mode='constant')
+        zero_padded_state[:-1,:-1] = game_state
 
         # check rows
         p, max_len = get_max_consecutive_row_length(zero_padded_state.ravel())
@@ -28,14 +30,13 @@ class ConnectFourGame(TurnBasedGame):
             return p
 
         # check diagonals
-        diagonals = np.concatenate([np.diagonal(zero_padded_state, offset=o) for o in range(-3, 4)])
+        diagonals = concat_diagonals(zero_padded_state)
         p, max_len = get_max_consecutive_row_length(diagonals)
         if max_len == 4:
             return p
 
         # check anti-diagonals
-        diagonals = np.concatenate([np.diagonal(np.rot90(zero_padded_state), offset=o)
-                                    for o in range(-4, 3)])
+        diagonals = concat_diagonals(np.fliplr(zero_padded_state))
         p, max_len = get_max_consecutive_row_length(diagonals)
         if max_len == 4:
             return p
@@ -52,13 +53,47 @@ class ConnectFourGame(TurnBasedGame):
         return np.all(diff >= 0)
 
 
+@njit
+def concat_diagonals(arr):
+    max_len = min(arr.shape)
+    num_items = 0
+
+    # count
+    for o in range(-3, 5):
+        for i in range(max_len):
+            if 0 <= i+o < arr.shape[1]:
+                num_items += 1
+
+    diags = np.zeros(num_items, dtype=np.int32)
+
+    c = 0
+    for o in range(-3, 5):
+        for i in range(max_len):
+            if 0 <= i+o < arr.shape[1]:
+                diags[c] = arr[i, i + o]
+                c += 1
+
+    return diags
+
+
+@njit
 def get_max_consecutive_row_length(flat_rows):
-    consecutive_row_lengths = ((k, sum(1 for _ in g)) for k, g in groupby(flat_rows)
-                               if k != 0)
-    try:
-        return max(consecutive_row_lengths, key=itemgetter(1))
-    except ValueError: # max() arg is an empty sequence
-        return 0, 0
+    max_len = 0
+    max_x = 0
+
+    cur_len = 0
+    cur_x = -1
+
+    for x in flat_rows:
+        if x != cur_x:
+            if cur_x != 0 and max_len < cur_len:
+                max_len = cur_len
+                max_x = cur_x
+            cur_len = 0
+            cur_x = x
+        cur_len += 1
+
+    return max_x, max_len
 
 
 def get_legal_positions(game_state):
@@ -101,17 +136,18 @@ def winning_or_random_move(game_state, p):
 
 if __name__ == '__main__':
     # game = ConnectFourGame()
-    # game.game_state = np.array([[0,0,0,1,0,0,0],
-    #                             [0,0,1,0,0,0,0],
-    #                             [0,1,0,0,0,0,0],
-    #                             [1,0,0,0,0,0,0],
+    # game.game_state = np.array([[0,0,0,0,0,0,0],
     #                             [0,0,0,0,0,0,0],
     #                             [0,0,0,0,0,0,0],
-    #                             [0,0,0,0,0,0,0]])
+    #                             [2,0,0,0,0,0,1],
+    #                             [0,2,0,0,0,1,0],
+    #                             [0,0,2,0,1,0,0],
+    #                             [0,0,0,2,0,0,0]])
     # print(game.victory_condition(game.game_state))
-
+    #
+    # exit(0)
 
     strats = [partial(winning_or_random_move, p=1),
-              partial(random_strategy, p=2)]
+              partial(winning_or_random_move, p=2)]
 
     stats = run_multiple_games(ConnectFourGame, strats, num_games=1000)
