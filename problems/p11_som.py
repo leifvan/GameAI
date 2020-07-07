4c518b992bdb120bba262aa0843ed3c1f34a6d4b
Leif Van Holland <holland@uni-bonn.de>
2020-07-07 11:26:05 +0200

    vectorize training functions and finalize scripts

--- problems/p11_som.py
+++ problems/p11_som.py
@@ -1,5 +1,6 @@
 import os
 import random
+from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,7 +15,7 @@ def load_path_csv(path):
     return np.array(items, dtype=np.float)
 
 
-def plot_data_and_som(data, weights, t):
+def plot_data_and_som(data, weights, t, export_name):
     fig = plt.figure()
     ax = Axes3D(fig)
     ax.scatter(data[:, 0], data[:, 1], data[:, 2], alpha=0.1, c='black', marker='o')
@@ -26,12 +27,21 @@ def plot_data_and_som(data, weights, t):
     plt.close()
 
 
+def dist_circle(i, j, k):
+    if i > j:
+        j, i = i, j
+    return min(abs(i - j), abs(k + i - j))
+
+
 def train_som(data, k, t_max, dist_fn, plot_fn, plot_every):
     def eta(t):
         return 1 - t / t_max
 
     def sigma(t):
         return np.exp(-t / t_max)
+
+    # precompute all distances and store in a matrix
+    dist = np.array([[dist_fn(i, j) for j in range(k)] for i in range(k)]) ** 2
 
     # initialize weights with random samples
     weights = np.array(random.sample(list(data), k))
@@ -41,9 +51,7 @@ def train_som(data, k, t_max, dist_fn, plot_fn, plot_every):
     for t in tqdm(range(t_max)):
         x = random.choice(data)
         i = np.argmin(np.linalg.norm(weights - x, axis=1))
-
-        for j in range(k):
-            weights[j] += eta(t) * np.exp(-dist_fn(i, j) / (2 * sigma(t))) * (x - weights[j])
+        weights += eta(t) * np.exp(-dist[i,...,None] / (2 * sigma(t))) * (x - weights)
 
         if (t + 1) % plot_every == 0:
             plot_fn(data, weights, t + 1)
@@ -53,60 +61,17 @@ def train_som(data, k, t_max, dist_fn, plot_fn, plot_every):
 
 
 if __name__ == '__main__':
-    path1 = load_path_csv("../data/q3dm1-path1.csv")
-    path2 = load_path_csv("../data/q3dm1-path2.csv")
-
-    k = 50
-    path = path1
-    export_name = f"plot_path1_k{k}"
-    os.makedirs(f"p11_results/{export_name}", exist_ok=True)
-
-
-    def dist(i, j):
-        if i > j:
-            j, i = i, j
-        return min(abs(i - j), abs(k + i - j))
-
+    paths = {'path1': load_path_csv("../data/q3dm1-path1.csv"),
+             'path2': load_path_csv("../data/q3dm1-path2.csv")}
 
     t_max = 20000
 
+    for path_name, path in paths.items():
+        for k in [5,10,20,50]:
+            print("run",path_name, k)
+            export_name = f"plot_{path_name}_k{k}"
+            os.makedirs(f"p11_results/{export_name}", exist_ok=True)
 
-    def plot_data_and_som(weights, t):
-        fig = plt.figure()
-        ax = Axes3D(fig)
-        ax.scatter(path[:, 0], path[:, 1], path[:, 2], alpha=0.1, c='black', marker='o')
-        ax.scatter(weights[:, 0], weights[:, 1], weights[:, 2], c='blue', marker='o')
-        ax.plot(weights[:, 0], weights[:, 1], weights[:, 2], c='blue', marker='o')
-        ax.plot(weights[(-1, 0), 0], weights[(-1, 0), 1], weights[(-1, 0), 2], c='blue', marker='o')
-        plt.title(f"k={len(weights)}, iteration {t}/{t_max}")
-        plt.savefig(f"p11_results/{export_name}/{export_name}_{t}.png")
-        plt.close()
-
-
-    def train_som(data, k, t_max, plot_fn, plot_every):
-        def eta(t):
-            return 1 - t / t_max
-
-        def sigma(t):
-            return np.exp(-t / t_max)
-
-        # initialize weights with random samples
-        weights = np.array(random.sample(list(data), k))
-
-        plot_fn(weights, 0)
-
-        for t in tqdm(range(t_max)):
-            x = random.choice(data)
-            i = np.argmin(np.linalg.norm(weights - x, axis=1))
-
-            for j in range(k):
-                weights[j] += eta(t) * np.exp(-dist(i, j) / (2 * sigma(t))) * (x - weights[j])
-
-            if (t + 1) % plot_every == 0:
-                plot_fn(weights, t + 1)
-
-        if t_max % plot_every != 0:
-            plot_fn(weights, t_max)
-
-
-    train_som(path, k, t_max, dist_fn=dist, plot_fn=plot_data_and_som, plot_every=1000)
+            train_som(path, k, t_max, dist_fn=partial(dist_circle, k=k),
+                      plot_fn=partial(plot_data_and_som, export_name=export_name),
+                      plot_every=1000)
--- problems/p17_som_2.py
+++ problems/p17_som_2.py
@@ -1,5 +1,6 @@
 import os
 import random
+from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,25 +8,16 @@ import scipy.spatial.distance
 from mpl_toolkits.mplot3d import Axes3D
 from tqdm import tqdm
 
-from p11_som import load_path_csv
-
-path = load_path_csv("../data/q3dm1-path2.csv")
-
-k = 20
-k2 = k // 2
-t_max = 20000
-export_name = f"plot_batch_k{k}"
-
-os.makedirs(f"p17_results/{export_name}", exist_ok=True)
+from p11_som import load_path_csv, train_som, dist_circle
 
 
-def dist_circle(i, j):
-    if i > j:
-        j, i = i, j
-    return min(abs(i - j), abs(k // 2 + i - j))
+# def dist_circle(i, j, k):
+#     if i > j:
+#         j, i = i, j
+#     return min(abs(i - j), abs(k // 2 + i - j))
 
 
-def plot_data_and_som(data, weights, t):
+def plot_data_and_som(data, weights, t, export_name):
     fig = plt.figure(figsize=(5, 5))
     ax = Axes3D(fig)
     ax.view_init(elev=45, azim=45)
@@ -40,41 +32,16 @@ def plot_data_and_som(data, weights, t):
     ax.plot(weights[(k2, 0), 0], weights[(k2, 0), 1], weights[(k2, 0), 2], c='blue', marker='o')
 
     plt.title(f"k={len(weights)}, iteration {t}/{t_max}")
-    # plt.show()
     plt.savefig(f"p17_results/{export_name}/{export_name}_{t}.png")
     plt.close()
 
 
-def dist(i, j):
-    if i < k2 and j < k2 or i >= k2 and j >= k2:  # both on one ring
-        return dist_circle(i, j)
-    else:  # on different rings
-        ci, cj = i // k2, (j // k2) * k2  # 0 if on left right, k2 if on right ring
-        return dist_circle(i, ci) + dist_circle(j, cj) + 1
-
-
-# ---------
-# task 17.1
-# ---------
-
-#train_som(path, k, t_max, dist_fn=dist, plot_fn=plot_data_and_som, plot_every=1000)
-
-# ---------
-# task 17.2
-# ---------
-
 def batch_train_som(data, k, t_max, dist_fn, plot_fn, plot_every):
-    # def eta(t):
-    #     return 1 - t / t_max
-
     def sigma(t):
         return np.exp(-t / t_max)
 
-    # def h(i, j, t):
-    #     return dist_fn(i, j) ** 2 / 2 / sigma(t)
-
     # precompute all distances and store in a matrix
-    dist = np.array([[dist_fn(i, j) for j in range(k)] for i in range(k)])
+    dist = np.array([[dist_fn(i, j) for j in range(k)] for i in range(k)]) ** 2
 
     # initialize weights with random samples
     weights = np.array(random.sample(list(data), k))
@@ -82,12 +49,9 @@ def batch_train_som(data, k, t_max, dist_fn, plot_fn, plot_every):
     plot_fn(data, weights, 0)
 
     for t in tqdm(range(t_max)):
-        # data (1000,3), weights (20,3) 1000x20
         indices = np.argmin(scipy.spatial.distance.cdist(data, weights), axis=1)
-        for i in range(k):
-            h = np.exp(-dist[indices, i] / (2 * sigma(t)))
-            weights[i] = np.sum(data * h[...,None], axis=0)
-            weights[i] /= np.sum(h)
+        h = np.exp(-dist[indices] / (2 * sigma(t)))
+        weights = np.sum(data[:, None] * h[..., None], axis=0) / np.sum(h, axis=0)[..., None]
 
         if (t + 1) % plot_every == 0:
             plot_fn(data, weights, t + 1)
@@ -96,4 +60,46 @@ def batch_train_som(data, k, t_max, dist_fn, plot_fn, plot_every):
         plot_fn(data, weights, t_max)
 
 
-batch_train_som(path, k, t_max, dist_fn=dist, plot_fn=plot_data_and_som, plot_every=1000)
+# batch_train_som(path, k, t_max, dist_fn=dist, plot_fn=plot_data_and_som, plot_every=1000)
+
+
+if __name__ == '__main__':
+    path = load_path_csv("../data/q3dm1-path2.csv")
+
+    for k in [6]:
+        print("k =",k)
+        k2 = k // 2
+        t_max = 20000
+        export_name = f"plot_k{k}"
+        os.makedirs(f"p17_results/{export_name}", exist_ok=True)
+
+
+        def dist(i, j):
+            if i < k2 and j < k2 or i >= k2 and j >= k2:  # both on one ring
+                return dist_circle(i, j, k2)
+            else:  # on different rings
+                ci, cj = i // k2, (j // k2) * k2  # 0 if on left right, k2 if on right ring
+                return dist_circle(i, ci, k2) + dist_circle(j, cj, k2) + 1
+
+
+        # ---------
+        # task 17.1
+        # ---------
+
+        export_name = f"plot_k{k}"
+        os.makedirs(f"p17_results/{export_name}", exist_ok=True)
+
+        train_som(path, k, t_max, dist_fn=dist,
+                  plot_fn=partial(plot_data_and_som, export_name=export_name),
+                  plot_every=1000)
+
+        # ---------
+        # task 17.2
+        # ---------
+
+        export_name = f"plot_batch_k{k}"
+        os.makedirs(f"p17_results/{export_name}", exist_ok=True)
+
+        batch_train_som(path, k, t_max, dist_fn=dist,
+                        plot_fn=partial(plot_data_and_som, export_name=export_name),
+                        plot_every=1000)
